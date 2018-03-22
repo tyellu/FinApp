c8167168ddf3235e9390ec2a6e67bc88a113365d
Tarun Yellu <tarunkumar.yellu@mail.utoronto.ca>
2018-03-22 00:29:47 -0400

    added service to preserve acc

---
+++ FinApp/API/services/accHistory.service.js
@@ -0,0 +1,25 @@
+var CronJob = require('cron').CronJob;
+import Portfolio from '../models/portfolio.model';
+import accHistory from '../models/accHistory.model';
+
+
+var accHist = new CronJob('00 0 0 * * 0-6', function() {
+    Portfolio.find({}, (err,portfolios) => {
+        portfolios.forEach((p) => {
+            // console.log("====P====");
+            // console.log(p);
+            accHistory.findOneAndUpdate(
+                {email: p.email},
+                {$setOnInsert:{email: p.email, history: [{accValue: Number(p.balance)}]}},
+                {safe: true, new: true, upsert: true},
+                function (err, acc){
+                    if(err) console.log(err);
+                    if(acc) console.log("account " + acc.email + " preserved");
+                }
+            );
+        });
+    });
+}, function(){console.log("accounts store");}, false, 'America/Toronto');
+
+
+export default accHist;
